9530529dfd2eb50223a401948e7c05358e8d3c48
MShizikU <sidorovstasd7@gmail.com>
2023-10-08 21:28:16 +0300

    Created plugs for PerfromUserInput functions

---
+++ src/main/java/org/example/tdd/PerformUserInput.java
@@ -0,0 +1,24 @@
+package org.example.tdd;
+
+import org.example.Equipment;
+
+/*Отработка результатов ввода
+ 1. Стирание выбранной буквы из массива алфавита;
+ 2. Проверка на наличие буквы в массиве слова;
+ 3. Добавление буквы в пользовательский массив
+ */
+public class PerformUserInput {
+
+    public Boolean isLetterInWord(Equipment equipment, Character userInput){
+        return null;
+    }
+
+    public Equipment removeLetterFromAlphabet(Equipment equipment, Character userInput){
+        return null;
+    }
+
+    public Equipment updateUserWord(Equipment equipment, Character userInput){
+        return null;
+    }
+
+}
