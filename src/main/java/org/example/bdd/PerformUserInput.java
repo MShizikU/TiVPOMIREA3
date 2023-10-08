3122003b2169e3c5b55f5daff5d8ee3a28051b63
MShizikU <sidorovstasd7@gmail.com>
2023-10-08 23:16:55 +0300

    Создано описание функций тестов cucumber для корректной работы Заглушки для функций Исправлены ошибки в функциях

---
+++ src/main/java/org/example/bdd/PerformUserInput.java
@@ -0,0 +1,22 @@
+package org.example.bdd;
+
+import org.example.Equipment;
+
+/*Отработка результатов ввода
+ 1. Стирание выбранной буквы из массива алфавита;
+ 2. Проверка на наличие буквы в массиве слова;
+ 3. Добавление буквы в пользовательский массив
+ */
+public class PerformUserInput {
+    public static Boolean isLetterInWord(Equipment equipment, Character userInput){
+        return null;
+    }
+
+    public static Equipment removeLetterFromAlphabet(Equipment equipment, Character userInput){
+        return null;
+    }
+
+    public static Equipment updateUserWord(Equipment equipment, Character userInput){
+        return null;
+    }
+}
--- src/test/java/test/bdd/PerformUserInputStepDef.java
+++ src/test/java/test/bdd/PerformUserInputStepDef.java
@@ -3,13 +3,43 @@ package test.bdd;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
 import org.example.Equipment;
+import org.example.bdd.PerformUserInput;
 
 import java.util.ArrayList;
 
-import static org.example.tdd.PerformUserInput.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PerformUserInputStepDef {
     private Equipment equipment;
+    private Character userInput;
+
+    @Дано("^загаданное слово (.*)")
+    public void givenCurrentWord(String currentWord) {
+        equipment = new Equipment();
+        equipment.alphabet = new ArrayList<>();
+        equipment.hiddenWord = new ArrayList<>();
+        equipment.userWord = new ArrayList<>();
+
+        for (char c : currentWord.toCharArray()) {
+            equipment.hiddenWord.add(c);
+            equipment.userWord.add('_');
+        }
+
+        for (int i = 1072; i <= 1103; i++) equipment.alphabet.add( (char) i );
+    }
+
+    @Когда("^пользователь вводит букву (.)")
+    public void givenInput(Character userInput){
+        this.userInput = userInput;
+    }
+
+    @Тогда("^проверка на наличие буквы возвращает (.*)")
+    public void checkIsLetterInWord(String strRes){
+        Boolean expResult = strRes.equals("да");
+        assertEquals(expResult, PerformUserInput.isLetterInWord(equipment, userInput));
+    }
 }
