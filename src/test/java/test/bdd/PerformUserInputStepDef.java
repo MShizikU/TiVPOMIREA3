b51e330ebfe7ccdc834fc689aa461a45e9ce48eb
MShizikU <sidorovstasd7@gmail.com>
2023-10-08 22:58:54 +0300

    BDD preparation

---
+++ src/test/java/test/bdd/PerformUserInputStepDef.java
@@ -0,0 +1,15 @@
+package test.bdd;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.example.Equipment;
+
+import java.util.ArrayList;
+
+import static org.example.tdd.PerformUserInput.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PerformUserInputStepDef {
+    private Equipment equipment;
+}
