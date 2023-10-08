9e0a4c1e961a22acd774c48bd30a903ca0231ba5
MShizikU <sidorovstasd7@gmail.com>
2023-10-08 21:10:04 +0300

    initial

---
+++ src/main/java/org/example/Equipment.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Equipment {
+
+    public ArrayList<Character> hiddenWord;
+    public ArrayList<Character> alphabet;
+    public ArrayList<Character> userWord;
+
+}
