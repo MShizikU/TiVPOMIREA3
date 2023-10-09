9879c458a5b095f19a564a46a7a6d888cc989c8f
Roman Fomichev <120161777+Romanfomic@users.noreply.github.com>
2023-10-09 14:17:15 +0300

    Добавление класса CheckEnteryString

---
+++ src/main/java/org/example/tdd/CheckEnteryString.java
@@ -0,0 +1,20 @@
+package org.example.tdd;
+import org.example.Equipment;
+
+/*Отработка пользовательского ввода
+ 1. Проверка на наличие буквы в алфавите;
+ 2. Проверка на корректность ввода;
+ 3. Приведение всех букв к нижнему регистру;
+ */
+
+public class CheckEnteryString {
+    public static boolean inAlphabet(Equipment equipment, Character userInput){
+        return false;
+    }
+    public static boolean isLetter(Equipment equipment, Character userInput){
+        return true;
+    }
+    public static Equipment toLower(Equipment equipment, Character userInput){
+        return equipment;
+    }
+}
