e1b775f50e4328dd9f416faecea5720a6fcce4fc
SuperCoon666 <voprishko03@gmail.com>
2023-10-10 17:09:39 +0300

    create ArrayDisplayer classes and bdd tests

---
+++ src/main/java/org/example/tdd/ArrayDisplayer.java
@@ -0,0 +1,14 @@
+package org.example.tdd;
+
+import java.util.ArrayList;
+
+public class ArrayDisplayer {
+
+    /**
+     * Выводит содержимое двух списков.
+     * @param userWord Список с символами слова пользователя.
+     * @param alphabet Список с алфавитом.
+     */
+    public void displayArrays(ArrayList<Character> userWord, ArrayList<Character> alphabet) {
+    }
+}
