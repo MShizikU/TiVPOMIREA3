9e3813bcbc19d0b2ad46d482341928f88cc392ac
SuperCoon666 <voprishko03@gmail.com>
2023-10-10 15:52:38 +0300

    update PoleChudes classes

--- src/main/java/org/example/tdd/PoleChudes.java
+++ src/main/java/org/example/tdd/PoleChudes.java
@@ -16,17 +16,44 @@ public class PoleChudes {
     }
 
     private void loadWordsFromFile(String filename) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(filename));
+        String currentCategory = "";
+
+        for (String line : lines) {
+            if (line.startsWith("[")) {
+                currentCategory = line.substring(1, line.length() - 1);
+                categories.put(currentCategory, new ArrayList<>());
+            } else {
+                categories.get(currentCategory).add(line);
+            }
+        }
     }
 
     public String chooseWord() {
-        return null;
+        List<String> allWords = new ArrayList<>();
+        for (List<String> categoryWords : categories.values()) {
+            allWords.addAll(categoryWords);
+        }
+        currentWord = allWords.get(random.nextInt(allWords.size()));
+        return currentWord;
     }
 
     public String chooseWordFromCategory(String category) {
+        if (categories.containsKey(category)) {
+            List<String> categoryWords = categories.get(category);
+            currentWord = categoryWords.get(random.nextInt(categoryWords.size()));
+            return currentWord;
+        }
         return null;
     }
 
     public static void main(String[] args) throws IOException {
+        PoleChudes game = new PoleChudes("words.txt");
+        String chosenWord = game.chooseWord();
+        System.out.println(chosenWord);
+        chosenWord = game.chooseWordFromCategory("города");
+        System.out.println(chosenWord);
     }
 }
+
 
